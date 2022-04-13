2865647813f04891221ed6798720b6516c9d5091
Jean-Giono <jeangionozehounkpe@yahoo.fr>
2022-04-13 04:50:14 +0200

    explanation of the algorithm added to the function

--- FL8_FunnyNumber.py
+++ FL8_FunnyNumber.py
@@ -5,7 +5,7 @@
               5 and 6 when written down in the usual way in the decimal system. We can
               enumerate all funny integers starting from the smallest one, i.e, : 5,6,55,56,65,etc.
                 
-              For a givenn number k, calculate the value of the k-th funny number in the sequence.
+              For a given number k, calculate the value of the k-th funny number in the sequence.
     
     
     Input : The input consists of a single positive integer k.
@@ -28,6 +28,20 @@
 '''
 
 def getFunnyNumber(n):
+    '''
+        Here is an algorithm that outputs the funny number quite fast :
+        for a given k, the goal is to calculate the value of the k-th
+        funny number.
+        
+        1. write k+1 in base 2 (k is in the decimal system)
+        2. remove initial 1, add 5 to all remaining digits
+        
+        For example for k = 10, k+1 = 11, in base 2 is equal to '1011'.
+        After removing the first 1, we have '011'. Then, 5 added to each of
+        the remaining digits gives : 566.
+        
+        Ref : The On-Line Encyclopedia Of Integer Sequences
+    '''
     bin_n = bin(n+1)
     funny_nb = int(bin_n[3:].replace('0','5').replace('1','6'))
     return funny_nb
